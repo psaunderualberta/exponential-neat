47d349242b91b5418b7b04a2859bdb9c6f3e2afa
Paul Saunders <psaunder@MacBook-Pro.local>
2024-03-28 15:26:54 -0600

    Wrote many tests, i *think* evaluate works & mutations

--- exponential_neat/genetic_algorithm/genome.py
+++ exponential_neat/genetic_algorithm/genome.py
@@ -1,13 +1,15 @@
 from genetic_algorithm.constants import OUTPUT_NODE_NAME
-from typing import Tuple
+from typing import Tuple, Any, List, Callable
 import pandas as pd
 import networkx as nx
 from copy import deepcopy
 from itertools import count
 import numpy as np
+from random import choice
 
 class Genome:
     global_innovation_number = count()
+    activ = lambda x: np.maximum(0, x)
     
     def __init__(self, num_features: int):
         self.num_features = num_features
@@ -19,22 +21,27 @@ class Genome:
         # Create the basic network
         net = nx.DiGraph()
         out_node_id = self.num_features
-        w_edges = [(i, out_node_id, {"weight": weight_gen(), "gin": i, "feature": i}) for i in range(self.num_features)]
-        net.add_weighted_edges_from(w_edges)
+        w_edges = [(i, out_node_id, {"weight": weight_gen(), "gin": i}) for i in range(self.num_features)]
+        net.add_edges_from(w_edges)
         nx.set_node_attributes(net, {out_node_id: True}, name=OUTPUT_NODE_NAME) 
-        print(net)
+        for i in range(self.num_features):
+            nx.set_node_attributes(net, {i: i for i in range(self.num_features)}, name="feature")
         return net
 
-    def clone(self):
-        return deepcopy(self)
+    @classmethod
+    def clone(cls, net: nx.DiGraph) -> nx.DiGraph:
+        return deepcopy(net)
 
     @classmethod
-    def newNode(cls, net: nx.DiGraph) -> nx.DiGraph:
-        edges = list(net.edges(data=True))
+    def newNode(cls, net: nx.DiGraph, edge2split: Tuple[int, int] | None = None) -> nx.DiGraph:
+        net = deepcopy(net)
+        edges: List[Tuple[int, int, dict[str, Any]]] = list(net.edges(data=True))
 
-        # Get random edge to split
-        i = np.random.randint(len(edges))
-        src_node, snk_node, data = edges[i]
+        if edge2split is None:
+            edge = choice(edges)
+        else:
+            edge = (*edge2split, net.edges[edge2split[0], edge2split[1]])
+        src_node, snk_node, data = edge 
         new_node = np.max(list(net.nodes())) + 1
  
         # Add the new edge
@@ -52,25 +59,41 @@ class Genome:
 
     @classmethod
     def newConnection(cls, net: nx.DiGraph) -> nx.DiGraph:
-        nodes = net.nodes()
-        edges = net.edges()
+        nodes = list(net.nodes())
+        edges = list(net.edges())
+        out_node = [node for node in net.nodes(data=True) if OUTPUT_NODE_NAME in node[1]][0]
+        input_node_ids = set(range(out_node[0]))
 
-        # There is
+        # Edge case where every connection already exists
+        for _ in range(1000):
+            n1 = choice(nodes)
+            n2 = choice(nodes)
 
-        return net
+            if n1 == n2 or (n1, n2) in edges or n2 in input_node_ids:
+                continue
+
+            nc = deepcopy(net)
+            nc.add_edge(n1, n2, weight=np.random.random())
+
+            if nx.is_directed_acyclic_graph(nc):
+                nx.set_edge_attributes(nc, {(n1, n2): {"gin": next(cls.global_innovation_number)}})
+                return nc
+
+        raise Exception("Cannot add to net and keep DAG nature")
 
     @classmethod
-    def evaluate(cls, net: nx.DiGraph, dataset: pd.DataFrame) -> np.array:
-        out_node = [node for node in net.nodes(data=True) if OUTPUT_NODE_NAME in node][0]
-        type(out_node)
+    def evaluate(cls, net: nx.DiGraph, dataset: np.array) -> np.array:
+        print(net.nodes(data=True))
+        print(net.edges(data=True))
+        out_node = [node for node in net.nodes(data=True) if OUTPUT_NODE_NAME in node[1]][0]
         return cls.__evaluate_helper(net, out_node, dataset) 
     
     @classmethod
-    def __evaluate_helper(cls, net: nx.DiGraph, out_node: Tuple[int, dict], dataset: pd.DataFrame) -> np.array:
+    def __evaluate_helper(cls, net: nx.DiGraph, out_node: Tuple[int, dict], dataset: np.array) -> np.array:
         # If the node is an input, just get the corresponding feature
         node_id, data_dict = out_node
         if "feature" in data_dict:
-            return dataset[data_dict["feature"]].to_numpy()
+            return dataset[:, data_dict["feature"]] 
 
         # else, compute all input nodes
         incoming_edges = net.in_edges(node_id, data=True)
@@ -78,14 +101,19 @@ class Genome:
         for edge in incoming_edges:
             assert edge[1] == node_id
 
+            # Skip disabled edges
+            if edge[2].get("disabled", False):
+                continue
+
             inp = cls.__evaluate_helper(net, (edge[0], net.nodes[edge[0]]), dataset)
-            weight: float = edge[2]["weight"]
+            weight = edge[2]["weight"]
             if np.prod(result.shape) == 0:
                 result = np.dot(inp, weight)
             else:
                 result += np.dot(inp, weight)
 
-        return result 
+        print(result)
+        return cls.activ(result) 
 
         
     @classmethod
--- exponential_neat/tests/test_genome.py
+++ exponential_neat/tests/test_genome.py
@@ -1,4 +1,6 @@
 from genetic_algorithm.genome import Genome
+import numpy as np
+import pytest
 import pandas as pd
 
 class TestGenome:
@@ -15,10 +17,88 @@ class TestGenome:
     def test_crossover(self):
         pass
     
-    def test_new_node(self):
-        pass
+    def test_new_node_set_edge(self):
+        G = Genome(3)
+       
+        net = G.newNet()
+        assert list(net.edges()) == [(0, 3), (1, 3), (2, 3)]
+
+        edge2split = (0, 3)
+        newnet = G.newNode(net, edge2split)
+
+        # Ensure the correct edge & node was added
+        assert set(newnet.nodes()) - set(net.nodes()) == set([4])
+        assert set(newnet.edges()) - set(net.edges()) == set([(0, 4), (4, 3)])
+        assert newnet.edges[0, 3].get("disabled", False) == True
+
+        old_weight = net.edges[0, 3]["weight"]
+        new_weight_first = newnet.edges[0, 4]["weight"]
+        new_weight_second = newnet.edges[4, 3]["weight"]
+
+        assert new_weight_first == 1.0
+        assert new_weight_second == old_weight 
+        
+
+    def test_new_node_random(self):
+        G = Genome(3)
+       
+        net = G.newNet()
+        assert list(net.edges()) == [(0, 3), (1, 3), (2, 3)]
+
+        newnet = G.newNode(net)
+
+        # Ensure the correct edge & node was added
+        assert set(newnet.nodes()) - set(net.nodes()) == set([4])
+
+        # Get the newly added node
+        new_edge_data = sorted(list(set(newnet.edges()) - set(net.edges())))
+        nsrc = new_edge_data[0][0]
+        nsnk = new_edge_data[1][1]
+
+        old_weight = net.edges[nsrc, nsnk]["weight"]
+        new_weight_first = newnet.edges[nsrc, 4]["weight"]
+        new_weight_second = newnet.edges[4, nsnk]["weight"]
+
+        assert new_weight_first == 1.0
+        assert new_weight_second == old_weight 
+        assert newnet.edges[nsrc, nsnk].get("disabled", False) == True
     
     def test_new_connection(self):
-        pass
-    
+        G = Genome(3)
+       
+        net = G.newNet()
+        assert list(net.edges()) == [(0, 3), (1, 3), (2, 3)]
 
+        net.remove_edge(2, 3)
+        newnet = G.newConnection(net)
+
+        assert len(list(newnet.nodes())) == len(list(net.nodes()))
+        assert len(newnet.edges()) == 1 + len(net.edges())
+        
+
+    def test_unable_to_add_connection(self):
+        G = Genome(2)
+
+        net = G.newNet()
+        net.add_edge(0, 1)
+       
+        with pytest.raises(Exception):
+            G.newConnection(net)
+    
+    def test_evaluate_simple(self): 
+        G = Genome(3)
+
+        data = np.array([
+            [1, 0, 1],
+            [-1, -1, -1],
+            [5, 3, 1]
+        ])
+
+        net = G.newNet(False)
+
+        result = G.evaluate(net, data)
+        assert np.all(result == np.array([2, 0, 9]))
+
+    def test_evaluate_complex(self):
+        # TODO
+        pass
